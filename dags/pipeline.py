36f4c7ee16e2a7b6e3b3eca9b1b932e0621d8340
cheewei <sohcheewei@u.nus.edu>
2026-06-08 14:53:55 +0800

    Wire parallel silver tasks into pipeline

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -10,14 +10,15 @@ R2_ENV = {
     "R2_ACCOUNT_ID": os.environ.get("R2_ACCOUNT_ID", ""),
     "R2_ACCESS_KEY_ID": os.environ.get("R2_ACCESS_KEY_ID", ""),
     "R2_SECRET_ACCESS_KEY": os.environ.get("R2_SECRET_ACCESS_KEY", ""),
+    "PYTHONPATH": "/app",
 }
 
 with DAG(
     dag_id="medallion_pipeline",
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime.datetime(2024, 1, 1),
     schedule="@monthly",
 ):
-    ingest_bronze = DockerOperator(
+    bronze_ingest = DockerOperator(
         task_id="ingest_bronze",
         image=IMAGE_NAME,
         command="python include/bronze/ingest_bronze.py --start-date {{ ds }} --end-date {{ ds }}",
@@ -26,10 +27,19 @@ with DAG(
         docker_url="unix://var/run/docker.sock",
     )
 
-    process_silver = DockerOperator(
-        task_id="process_silver",
+    silver_process_legal_docs = DockerOperator(
+        task_id="process_silver_legal_docs",
         image=IMAGE_NAME,
-        command="python include/silver/sample_silver.py",
+        command="python include/silver/process_legal_docs.py",
+        environment=R2_ENV,
+        auto_remove="force",
+        docker_url="unix://var/run/docker.sock",
+    )
+
+    silver_process_wiki_docs = DockerOperator(
+        task_id="process_silver_wiki_docs",
+        image=IMAGE_NAME,
+        command="python include/silver/process_wiki_docs.py",
         environment=R2_ENV,
         auto_remove="force",
         docker_url="unix://var/run/docker.sock",
@@ -44,4 +54,4 @@ with DAG(
         docker_url="unix://var/run/docker.sock",
     )
 
-    ingest_bronze >> process_silver >> process_gold
+    bronze_ingest >> [silver_process_legal_docs, silver_process_wiki_docs] >> process_gold
