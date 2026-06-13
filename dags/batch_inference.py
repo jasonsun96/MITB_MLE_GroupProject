b60fcc45056ddc4024b20e1142d7621e53020783
cheewei <sohcheewei@u.nus.edu>
2026-06-13 22:50:14 +0800

    added dag for batch inference

---
+++ dags/batch_inference.py
@@ -0,0 +1,49 @@
+import datetime
+import os
+
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.sdk import DAG
+
+IMAGE_NAME = "document_topic_tagger"
+
+DEFAULT_ARGS = {
+    "retries": 2,
+    "retry_delay": datetime.timedelta(minutes=5),
+    "retry_exponential_backoff": True,
+    "max_retry_delay": datetime.timedelta(minutes=30),
+}
+
+R2_ENV = {
+    "R2_ACCOUNT_ID": os.environ.get("R2_ACCOUNT_ID", ""),
+    "R2_ACCESS_KEY_ID": os.environ.get("R2_ACCESS_KEY_ID", ""),
+    "R2_SECRET_ACCESS_KEY": os.environ.get("R2_SECRET_ACCESS_KEY", ""),
+    "PYTHONPATH": "/app",
+}
+
+COMMON = dict(
+    image=IMAGE_NAME,
+    environment=R2_ENV,
+    auto_remove="force",
+    docker_url="unix://var/run/docker.sock",
+)
+
+with DAG(
+    dag_id="batch_inference_pipeline",
+    start_date=datetime.datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    max_active_runs=1,
+    default_args=DEFAULT_ARGS,
+    params={"canary_percentage": 5.0, "input_path": ""},
+):
+    run_batch_inference = DockerOperator(
+        task_id="run_batch_inference",
+        command=(
+            "python include/inference/batch_inference.py "
+            "--batch-id {{ ts_nodash }} "
+            "--canary-percentage {{ params.canary_percentage }} "
+            "{% if params.input_path %}--input-path '{{ params.input_path }}'{% endif %}"
+        ),
+        execution_timeout=datetime.timedelta(hours=4),
+        **COMMON,
+    )
